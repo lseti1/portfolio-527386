95bcb5d9afcb5b5e315f5969ed5b736053a19847
Lucas Setiady <luca.setia@gmail.com>
2025-01-21 20:47:16 +1000

    Visual Improvements for Navigation Bar and Changing Text Boxes for different pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const updateContent = () => {
+    switch (currentPage) {
+      case "home":
+        return <p>"I'm a 3rd year Computer Science student majoring in Software Development, studying online at Griffith University. I am currently looking
+        for Internships and would greatly appreciate any considerations to take me in as a team member. Please feel free to navigate around my digital portfolio
+        to see the projects I've worked on, along with my personal contact information/social networks.</p>;
+      case "more":
+        return <p>More About Me!</p>;
+      case "projects":
+        return <p>The two projects I have worked on are a simple yearly planner, blah blah</p>;
+      case "resume":
+        return <p>Here are links to my Resume online -</p>;
+      default:
+        return <p>No page found!</p>;
+    }
+  }
+
+  const updateTitle = () => {
+    switch (currentPage) {
+      case "home":
+        return <h1>Hi, my name is Lucas Setiady</h1>;
+      case "more":
+        return <h1>More about myself:</h1>;
+      case "projects":
+        return <h1>My personal projects: </h1>;
+      case "resume":
+        return <h1>My Resume</h1>;
+      default:
+        return <h1>No page found!</h1>;
+    }
+  }
+
   return (
     <div className="App">
       <div className="geometricBackground"></div>
-      <div className="greetingName">Hi, my name is Lucas Setiady</div>
-      <div className="aboutMe">
-        I'm a 3rd year Computer Science student majoring in Software Development, studying online at Griffith University. I am currently looking
-        for Internships and would greatly appreciate any considerations to take me in as a team member. Please feel free to navigate around my digital portfolio
-        to see the projects I've worked on, along with my personal contact information/social networks. 
+      <div className="greetingName">{updateTitle()}</div>
+      <div className="aboutMe">{updateContent()}
       </div>
       <div className="myPhoto"></div>
       <div className="navigationBar">
-        <div className="navigationItem">Home</div>
-        <div className="navigationItem">More About Me</div>
-        <div className="navigationItem">Projects</div>
-        <div className="navigationItem">Resume</div>
+        <div className={currentPage === "home" ? "active" : "navigationItem"} onClick={() => handlePageChange("home")}>Home</div>
+        <div className={currentPage === "more" ? "active" : "navigationItem"} onClick={() => handlePageChange("more")}>More About Me</div>
+        <div className={currentPage === "projects" ? "active" : "navigationItem"} onClick={() => handlePageChange("projects")}>Projects</div>
+        <div className={currentPage === "resume" ? "active" : "navigationItem"} onClick={() => handlePageChange("resume")}>Resume</div>
       </div> 
-      <div className="contactInfo">Contact Info</div>
+      <div className="contactInfo">
+        <p>Contact Info</p>
+      </div>
       <div className="logo"></div>
       <div className="linkedIn"></div>
       <div className="GitHub"></div>
