be94d4210755b4c5271c1dcff0f61ef9a218fa5f
Lucas Setiady <luca.setia@gmail.com>
2025-01-21 17:56:28 +1000

    Updated Background and Layout, also imported images for various componenets

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="geometricBackground"></div>
       <div className="greetingName">Hi, my name is Lucas Setiady</div>
-      <div className="aboutMe">About Me</div>
-      <div className="myPhoto">This is a photo of me</div>
+      <div className="aboutMe">
+        I'm a 3rd year Computer Science student majoring in Software Development, studying online at Griffith University. I am currently looking
+        for Internships and would greatly appreciate any considerations to take me in as a team member. Please feel free to navigate around my portfolio
+        to see the projects I've worked on as well as find my personal contact information/social networks. 
+      </div>
+      <div className="myPhoto"></div>
       <div className="navigationBar">Navigation Bar: More About Me, Projects, Resume</div>
-      <div className="contactInfo">Button for Contact Info Pop-Up</div>
-      <div className="logo">Logo Photo</div>
-      <div className="linkedIn">Link to LinkedIn</div>
-      <div className="GitHub">Link to GitHub</div>
+      <div className="contactInfo">Contact Info</div>
+      <div className="logo"></div>
+      <div className="linkedIn"></div>
+      <div className="GitHub"></div>
     </div>
   );
 }
