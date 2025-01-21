5d615c7365d49ca9d953c5bf1f12dfb5f4136934
Lucas Setiady <luca.setia@gmail.com>
2025-01-21 20:17:41 +1000

    Updated More Visuals and Set Up Navigation Bar

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ function App() {
       <div className="greetingName">Hi, my name is Lucas Setiady</div>
       <div className="aboutMe">
         I'm a 3rd year Computer Science student majoring in Software Development, studying online at Griffith University. I am currently looking
-        for Internships and would greatly appreciate any considerations to take me in as a team member. Please feel free to navigate around my portfolio
-        to see the projects I've worked on as well as find my personal contact information/social networks. 
+        for Internships and would greatly appreciate any considerations to take me in as a team member. Please feel free to navigate around my digital portfolio
+        to see the projects I've worked on, along with my personal contact information/social networks. 
       </div>
       <div className="myPhoto"></div>
-      <div className="navigationBar">Navigation Bar: More About Me, Projects, Resume</div>
+      <div className="navigationBar">
+        <div className="navigationItem">Home</div>
+        <div className="navigationItem">More About Me</div>
+        <div className="navigationItem">Projects</div>
+        <div className="navigationItem">Resume</div>
+      </div> 
       <div className="contactInfo">Contact Info</div>
       <div className="logo"></div>
       <div className="linkedIn"></div>
