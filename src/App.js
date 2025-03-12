7359be9649099dda20cd4bf8c44e73e8b620b768
Lucas Setiady <luca.setia@gmail.com>
2025-03-12 14:12:24 +1000

    Additional CSS Changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [currentPage, setCurrentPage] = useState("home");
+  const [isVisible, setIsVisible] = useState(true);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
