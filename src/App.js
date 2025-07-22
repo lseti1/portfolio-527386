d6e24b20dd67f10de7571f7c1ed878fe4c2be02e
Lucas Setiady <luca.setia@gmail.com>
2025-07-22 15:15:36 +1000

    Functionality: Adding titles to links/images

--- src/App.js
+++ src/App.js
@@ -34,10 +34,10 @@ function App() {
 					<div className="ApplicationImage" >
 						<button onClick={decrement} className="ApplicationsCarouselButton">&lt;</button>
 						<div className="ImageWrapper">
-							<a href="https://planner773629-tau-three.vercel.app/" target="_blank" rel="noopener noreferrer">
+							<a href="https://planner773629-tau-three.vercel.app/" target="_blank" rel="noopener noreferrer" title="Planner App">
 								<img className="SourceImage" src="/lifePlanner.png" alt="LifePlannerApp"/>
 							</a>
-							<a className="GitHubLink" href="https://github.com/lseti1/lifePlanner-773629" target="_blank" rel="noopener noreferrer">
+							<a className="GitHubLink" href="https://github.com/lseti1/lifePlanner-773629" target="_blank" rel="noopener noreferrer" title="Planner Repository">
 								<img className="GitHubImage" src="/GitHub Logo.png" />
 							</a>
 						</div>
@@ -55,7 +55,7 @@ function App() {
 						<button onClick={decrement} className="ApplicationsCarouselButton">&lt;</button>
 						<div className="ImageWrapper">
 							<img className="SourceImage" src="/JobRecorderNet.png" alt="LifePlannerApp"/>
-							<a className="GitHubLink" href="https://github.com/MatchaBreak/JobRecorderNet" target="_blank" rel="noopener noreferrer">
+							<a className="GitHubLink" href="https://github.com/MatchaBreak/JobRecorderNet" target="_blank" rel="noopener noreferrer"  title="JobRecorderNet Repository">
 								<img className="GitHubImage" src="/GitHub Logo.png" />
 							</a>
 						</div>
@@ -73,10 +73,10 @@ function App() {
 					<div className="ApplicationImage" >
 						<button onClick={decrement} className="ApplicationsCarouselButton">&lt;</button>
 						<div className="ImageWrapper">
-							<a href="https://holidaysearcher198302.vercel.app/" target="_blank" rel="noopener noreferrer">
+							<a href="https://holidaysearcher198302.vercel.app/" target="_blank" rel="noopener noreferrer" title="Plan Finder App">
 								<img className="SourceImage" src="/planFinder.png" alt="PlanFinderApp"/>
 							</a>
-							<a className="GitHubLink" href="https://github.com/lseti1/planFinder-198302" target="_blank" rel="noopener noreferrer">
+							<a className="GitHubLink" href="https://github.com/lseti1/planFinder-198302" target="_blank" rel="noopener noreferrer"  title="Plan Finder Repository">
 								<img className="GitHubImage" src="/GitHub Logo.png" />
 							</a>
 						</div>
@@ -93,10 +93,10 @@ function App() {
 					<div className="ApplicationImage" >
 						<button onClick={decrement} className="ApplicationsCarouselButton">&lt;</button>
 						<div className="ImageWrapper">
-							<a href="https://sky-cast-853920.vercel.app/" target="_blank" rel="noopener noreferrer">
+							<a href="https://sky-cast-853920.vercel.app/" target="_blank" rel="noopener noreferrer" title="Weather App">
 								<img className="SourceImage" src="/skycast.png" alt="SkycastApp"/>
 							</a>
-							<a className="GitHubLink" href="https://github.com/lseti1/skyCast-853920" target="_blank" rel="noopener noreferrer">
+							<a className="GitHubLink" href="https://github.com/lseti1/skyCast-853920" target="_blank" rel="noopener noreferrer" title="Weather App Repository">
 								<img className="GitHubImage" src="/GitHub Logo.png" />
 							</a>
 						</div>
@@ -128,16 +128,16 @@ function App() {
 				<div className="ContentWrapper AboutMeContent">
 					<div className="AboutMeLeft">
 						<div className="PortraitWrapper">
-							<img className="Portrait" src="/PotraitMySelf.jpg" alt="PortraitOfSelf" />
+							<img className="Portrait" src="/PotraitMySelf.jpg" alt="PortraitOfSelf" title="Display Picture"/>
 						</div>
 						<div className="QuickLinks">
-							<a href="https://github.com/lseti1" target="_blank" rel="noopener noreferrer">
+							<a href="https://github.com/lseti1" target="_blank" rel="noopener noreferrer" title="Personal GitHub Profile">
 								<img className="Icon" src="/GitHub Logo.png" alt="GitHubLink" />
 							</a>
-							<a href="https://www.linkedin.com/in/lucassetiady/" target="_blank" rel="noopener noreferrer">
+							<a href="https://www.linkedin.com/in/lucassetiady/" target="_blank" rel="noopener noreferrer" title="Personal LinkedIn Profile">
 								<img className="Icon" src="/LinkedIn Logo.jpg" alt="LinkedInLink" />
 							</a>
-							<a href="mailto:luca.setia@gmail.com" target="_blank" rel="noopener noreferrer">
+							<a href="mailto:luca.setia@gmail.com" target="_blank" rel="noopener noreferrer" title="Personal Email">
 								<img className="Icon" src="/mailIcon.png" alt="MailLink" />
 							</a>
 						</div>
@@ -161,27 +161,27 @@ function App() {
 				<h1 className="ContentWrapper ContentTitle" style={{ color: 'white' }}>Website Designs</h1>
 				<div className="ContentWrapper WebsiteDesignsContent">
 					<div className="WebDesignCard">
-						<a href="https://e-commerce-design-202501.vercel.app/" target="_blank" rel="noopener noreferrer">
+						<a href="https://e-commerce-design-202501.vercel.app/" target="_blank" rel="noopener noreferrer" title="eCommerce Tech Store Web Design">
 							<img className="SourceImage" src="/eCommerceDesign1.png" alt="eCommerceWebDesign1"/>
 						</a>
-						<a className="WebGitHubLink" href="https://github.com/lseti1/eCommerceDesign-202501" target="_blank" rel="noopener noreferrer">
+						<a className="WebGitHubLink" href="https://github.com/lseti1/eCommerceDesign-202501" target="_blank" rel="noopener noreferrer" title="eCommerce Tech Design Repository">
 							<img className="GitHubImage" src="/GitHub Logo.png" />
 						</a>
 						<p className="WebDesignDescription">This is a multi-page eCommerce design for a Tech store (made with HTML/Tailwind CSS) including large Hero Banners,
 							multi-section pages and a number of accessible pages to simulate a fully functional eCommerce store.</p>
 					</div>
 					<div className="WebDesignCard">
-						<a href="https://e-commerce-design-202502.vercel.app/" target="_blank" rel="noopener noreferrer">
+						<a href="https://e-commerce-design-202502.vercel.app/" target="_blank" rel="noopener noreferrer" title="eCommerce Music Store Web Design">
 							<img className="SourceImage" src="/eCommerceDesign2.png" alt="eCommerceWebDesign2"/>
 						</a>
-						<a className="WebGitHubLink" href="https://github.com/lseti1/eCommerceDesign-202502" target="_blank" rel="noopener noreferrer">
+						<a className="WebGitHubLink" href="https://github.com/lseti1/eCommerceDesign-202502" target="_blank" rel="noopener noreferrer" title="eCommerce Music Design Repository">
 							<img className="GitHubImage" src="/GitHub Logo.png" />
 						</a>
 						<p className="WebDesignDescription">This is an eCommerce design for a multi-page Music store (made with HTML/Tailwind CSS) including Hero Banners,
 							product cards and other e-commerce visuals. Designed with a music-inspired theme and a number of different banners.</p>
 					</div>
 					<div className="WebDesignCard">
-						<a href="https://lucas-websitedesign1.in/" target="_blank" rel="noopener noreferrer">
+						<a href="https://lucas-websitedesign1.in/" target="_blank" rel="noopener noreferrer" title="Tourism Web Design">
 							<img className="SourceImage" src="/elementorDesign1.png" alt="ElementorWebDesign1"/>
 						</a>
 						<p className="WebDesignDescription">This is a tourism website for the city of London (designed using WordPress and Elementor). It features a sleek,
@@ -189,7 +189,7 @@ function App() {
 							engaging and informative experience for visitors.</p>
 					</div>
 					<div className="WebDesignCard">
-						<a href="https://lucas.websitedesign2.in/" target="_blank" rel="noopener noreferrer">
+						<a href="https://lucas.websitedesign2.in/" target="_blank" rel="noopener noreferrer" title="Restaurant Web Design">
 							<img className="SourceImage" src="/elementorDesign2.png" alt="ElementorWebDesign2"/>
 						</a>
 						<p className="WebDesignDescription">This is a restaurant website for an authentic Japanese dining experience (crafted in WordPress using Elementor).
@@ -197,17 +197,17 @@ function App() {
 							special offers, and seasonal events, offering an immersive and appetizing introduction to the cuisine.</p>
 					</div>
 					<div className="WebDesignCard">
-						<a href="https://portfolio-design-478053.vercel.app/" target="_blank" rel="noopener noreferrer">
+						<a href="https://portfolio-design-478053.vercel.app/" target="_blank" rel="noopener noreferrer" title="Photography Portfolio Design">
 							<img className="SourceImage" src="/portfolioDesign.png" alt="WebDesign1"/>
 						</a>
-						<a className="WebGitHubLink" href="https://github.com/lseti1/portfolioDesign-478053" target="_blank" rel="noopener noreferrer">
+						<a className="WebGitHubLink" href="https://github.com/lseti1/portfolioDesign-478053" target="_blank" rel="noopener noreferrer" title="Photography Portfolio Repository">
 							<img className="GitHubImage" src="/GitHub Logo.png" />
 						</a>
 						<p className="WebDesignDescription">This is a web-design for a photographer's Digital Portfolio (Made with HTML/CSS) including multiple accessible sections
 							with a user-friendly layout that utilises various graphics & animations for visual flair. </p>
 					</div>
 					<div className="WebDesignCard">
-						<a>
+						<a title="Former Portfolio Design">
 							<img className="SourceImage" src="/OlderPortfolioDesign.png" alt="OlderPortfolioWebDesign"/>
 						</a>
 						<a className="WebGitHubLink" href="" target="_blank" rel="noopener noreferrer">
@@ -228,7 +228,7 @@ function App() {
 								<h1>Bachelor's Degree</h1>
 								<p>Major: Software Development</p>
 							</div>
-							<a className="DocumentLink" href="https://drive.google.com/file/d/1maTIF8UQtCtTLNpNfdWNWqwi2w337snM/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+							<a className="DocumentLink" href="https://drive.google.com/file/d/1maTIF8UQtCtTLNpNfdWNWqwi2w337snM/view?usp=sharing" target="_blank" rel="noopener noreferrer" title="Certificate">
 								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M8 16h8M8 12h8m-6 8h6a2 2 0 002-2V7a2 2 0 00-.59-1.41l-4-4A2 2 0 0012 1H6a2 2 0 00-2 2v16a2 2 0 002 2h2" />
 								</svg>
@@ -240,7 +240,7 @@ function App() {
 								<h1>Academic Transcript</h1>
 								<p>2x Griffith Academic Excellence Award Recipient</p>
 							</div>
-							<a className="DocumentLink" href="https://drive.google.com/file/d/1csel03zxJA76Y9ueQyO46-9wynmcfIGV/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+							<a className="DocumentLink" href="https://drive.google.com/file/d/1csel03zxJA76Y9ueQyO46-9wynmcfIGV/view?usp=sharing" target="_blank" rel="noopener noreferrer" title="Transcript">
 								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M8 16h8M8 12h8m-6 8h6a2 2 0 002-2V7a2 2 0 00-.59-1.41l-4-4A2 2 0 0012 1H6a2 2 0 00-2 2v16a2 2 0 002 2h2" />
 								</svg>
@@ -251,7 +251,7 @@ function App() {
 								<p className="CardTimeline">Last Updated: 14/07/25</p>
 								<h1>Digital Resume</h1>
 							</div>
-							<a className="DocumentLink" href="https://drive.google.com/file/d/1EVnFoxOuXq_0qvpaLY5eHc02KuJm6Rao/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+							<a className="DocumentLink" href="https://drive.google.com/file/d/1EVnFoxOuXq_0qvpaLY5eHc02KuJm6Rao/view?usp=sharing" target="_blank" rel="noopener noreferrer" title="Resume">
 								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M8 16h8M8 12h8m-6 8h6a2 2 0 002-2V7a2 2 0 00-.59-1.41l-4-4A2 2 0 0012 1H6a2 2 0 00-2 2v16a2 2 0 002 2h2" />
 								</svg>
